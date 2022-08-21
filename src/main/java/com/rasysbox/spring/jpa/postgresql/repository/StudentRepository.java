472f5f7617a3cf213da7eebc3c4cac790616e9a4
ALfredo Noguera <alfnogrua@gmail.com>
2022-08-21 16:37:12 -0500

    1er commit

--- src/main/java/com/rasysbox/spring/jpa/postgresql/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rasysbox.spring.jpa.postgresql.repository;
-
-import com.rasysbox.spring.jpa.postgresql.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("studentRepository")
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
